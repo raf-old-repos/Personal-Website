e10b012d3ffc8f40056ada4822981f6fcf285424
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-08-28 22:54:56 -0400

    fixed responsiveness bug

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,7 +15,7 @@ interface Props {
 const Card: React.FC<Props> = ({ mainheader, links, iconURL }) => {
   return (
     <React.Fragment>
-      <div className="flex flex-col justify-center items-center h-screen border-4 dark:border-white border-black md:h-screen rounded-lg dark:text-white text-black ">
+      <div className="flex flex-col justify-center items-center h-screen w-screen border-4 dark:border-white border-black md:h-screen rounded-lg dark:text-white text-black ">
         <div className="md:flex md:flex-row md:pt-40 pt-20">
           <div className="flex flex-col justify-center items-center  md:flex md:flex-cols md:justify-center md:items-center md:pl-36">
             {/* @ts-ignore */}
@@ -36,10 +36,10 @@ const Card: React.FC<Props> = ({ mainheader, links, iconURL }) => {
             {mainheader}
           </h1>
         </div>
-        <div className="md:flex md:flex-row md:justify-center md:pr-10 md:pt-5 text-4xl ">
+        <div className="mb-36 md:flex md:flex-row md:justify-center md:pr-10 md:pt-5 text-4xl ">
           <ul className="flex flex-row flex-wrap justify-center md:flex md:flex-row ">
             {links.map((link, idx) => (
-              <li key={idx} className="p-5 md:px-5">
+              <li key={idx} className="px-5 md:px-5">
                 <LinkButton name={link.name} link={link.url} icon={link.icon} />
               </li>
             ))}
