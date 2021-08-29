591e36ac925b2528c86c37cf6e1b94470cab92fa
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-08-28 21:55:23 -0400

    tilt > no tilt

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { RiDiscordLine } from "react-icons/ri";
-import { FiGithub } from "react-icons/fi"
-import { FaFacebook } from "react-icons/fa"
+import { FiGithub } from "react-icons/fi";
+import { FaFacebook } from "react-icons/fa";
+import dynamic from "next/dynamic";
+// import your component with dynamic and disable SSR
+const Tilty = dynamic(() => import("react-tilty"), { ssr: false });
 
 interface Props {
   name: string;
@@ -14,21 +17,24 @@ interface Props {
 const LinkButton: React.FC<Props> = ({ name, link, icon }) => {
   return (
     <React.Fragment>
-      {(() => {
-        switch (icon) {
-          case "insta":
-            return <AiOutlineInstagram />;
-          case "discord":
-            return <RiDiscordLine />;
-          case "github":
-              return <FiGithub />
-          case "facebook":
-              return <FaFacebook />
-        }
-      })()}
-      <Link href={link}>
-        <a className="text-xl">{name}</a>
-      </Link>
+      {/* @ts-ignore */}
+      <Tilty max={40}>
+        {(() => {
+          switch (icon) {
+            case "insta":
+              return <AiOutlineInstagram />;
+            case "discord":
+              return <RiDiscordLine />;
+            case "github":
+              return <FiGithub />;
+            case "facebook":
+              return <FaFacebook />;
+          }
+        })()}
+        <Link href={link}>
+          <a className="text-xl">{name}</a>
+        </Link>
+      </Tilty>
     </React.Fragment>
   );
 };
