20a9b070b9703011eb593a074a7a0c2f158af833
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-08-28 23:06:58 -0400

    I hate safari with passion

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ const Home = () => {
         />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
