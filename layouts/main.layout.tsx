fb9158ac9513e88e130597dcadf74cc89678b338
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-08-28 23:14:16 -0400

    final try for safari

--- layouts/main.layout.tsx
+++ layouts/main.layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const MainLayout = ({ children }) => {
   return (<React.Fragment>
-      <div className="dark:bg-black bg-white">
+      <div className="dark:bg-black bg-white overflow-hidden">
           {children}
       </div>
       </React.Fragment>);
